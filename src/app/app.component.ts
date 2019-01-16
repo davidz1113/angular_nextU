ca53577d4a1fa43aa75f2b7bb906106667cee86b
Andrés Carvajal <d.carvajal_13@hotmail.com>
2019-01-15 23:37:51 -0500

    implementando nuevos cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NuevaTareaComponent } from './nueva-tarea/nueva-tarea.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 't-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
 }
